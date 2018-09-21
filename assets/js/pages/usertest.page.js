820ac3db7ba2d029f618dc178a708e97a4aa7e77
YouruiGuo <yourui@ualberta.ca>
2018-09-20 19:44:34 -0600

    ui modified

--- assets/js/pages/usertest.page.js
+++ assets/js/pages/usertest.page.js
@@ -1,17 +1,18 @@
 parasails.registerPage('usertest', {
 
 	data: {
-	
+
 	},
 
 	beforeMount: function(){
 		_.extend(this, window.SAILS_LOCALS);
-		
-		
+
+
 	},
 
 	mounted: function(){
 		this._timerButton();
+		this.hide();
 
 	},
 
@@ -20,13 +21,21 @@ parasails.registerPage('usertest', {
 		_timerButton: function(){
 			$('#nextButton').attr('disabled','disabled').css({'background-color':'grey'});
 			setTimeout(this.enableButton, 20000);
-			
+
 		},
 
 		enableButton: function(){
 			$('#nextButton').removeAttr('disabled').css({'background-color':'black'});
+		},
+
+		unhide: function(){
+			$('#answer').css("display", "block");
+		},
+
+		hide: function(){
+			$('#answer').css("display", "none");
+
 		}
-		
 
 	}
 });
